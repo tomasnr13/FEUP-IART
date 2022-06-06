37895461e9db2b285b97308b08758dac1fcb1b74
tomasnr13 <up201906743@fe.up.pt>
2022-06-06 09:37:43 +0100

    code finished but still needs testing

--- proj2/src/main.py
+++ proj2/src/main.py
@@ -1,80 +1,138 @@
-import nltk
-'''import tensorflow as tf'''
 import pandas as pd
-import numpy as np
-'''
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
-from tensorflow.keras.layers import Dropout
-from tensorflow.keras.layers import Flatten
-from tensorflow.keras.layers import Conv1D
-from tensorflow.keras.layers import MaxPooling1D
-from tensorflow.keras.layers import Embedding
-from tensorflow.keras.preprocessing.text import Tokenizer
-from tensorflow.keras.preprocessing import sequence
-'''
-from sklearn.preprocessing import LabelEncoder
-
-
-#Charts
-from sklearn import metrics 
-from sklearn.metrics import f1_score, accuracy_score,confusion_matrix,classification_report
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-# Time
-import time
-import datetime
-
-#Performance Plot
-import contractions
-from bs4 import BeautifulSoup
-import numpy as np
 import re
-import tqdm
-import unicodedata
-
-#nltk.download('punkt')
-
-
-# fix random seed for reproducibility
-seed = 3541
-np.random.seed(seed)
-
-
-
-# Pre-Processing Function
-
-def strip_html_tags(text):
-  soup = BeautifulSoup(text, "html.parser")
-  [s.extract() for s in soup(['iframe', 'script'])]
-  stripped_text = soup.get_text()
-  stripped_text = re.sub(r'[\r|\n|\r\n]+', '\n', stripped_text)
-  return stripped_text
-
-def remove_accented_chars(text):
-  text = unicodedata.normalize('NFKD', text).encode('ascii', 'ignore').decode('utf-8', 'ignore')
-  return text
-
-def pre_process_corpus(docs):
-  norm_docs = []
-  for doc in tqdm.tqdm(docs):
-    doc = strip_html_tags(doc)
-    doc = doc.translate(doc.maketrans("\n\t\r", "   "))
-    doc = doc.lower()
-    doc = remove_accented_chars(doc)
-    doc = contractions.fix(doc)
-    # lower case and remove special characters\whitespaces
-    doc = re.sub(r'[^a-zA-Z0-9\s]', '', doc, re.I|re.A)
-    doc = re.sub(' +', ' ', doc)
-    doc = doc.strip()  
-    norm_docs.append(doc)
-  
-  return norm_docs
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem.snowball import SnowballStemmer
+from nltk.stem.porter import PorterStemmer
+from nltk.stem import WordNetLemmatizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import GaussianNB
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import tree
+from sklearn.svm import SVC
+from sklearn.neighbors import NearestNeighbors
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import f1_score
+import time
 
 
+train = pd.read_csv("./resources/train.csv")
+#test = pd.read_csv("./resources/test.csv")
+tokens=[]
+corpus=[]
 
-#Reading DAtaset from csv files to Pandas dataframes
+#cleaning and tokenizing 
+def pre_process():
+    tokens=[]
+    for i in range(0,train.shape[0]):
+        # remove non alphabetic chars
+        title = re.sub('[^a-zA-Z]', ' ', train['review_title'][i])
+        review = re.sub('[^a-zA-Z]', ' ', train['review_text'][i])
+        #all words to lower
+        title=title.lower
+        review = review.lower
+        #remove stop words
+        titlearr = []
+        for word in title.split:
+            if word not in stopwords.words('english'):
+                titlearr.append(word)
+        reviewarr = []
+        for word in review.split:
+            if word not in stopwords.words('english'):
+                reviewarr.append(word)
+        tokens.append((titlearr,reviewarr))
 
-dataset_train = pd.read_csv('./resources/train.csv')
-dataset_test = pd.read_csv('./resources/test.csv')
+
+#stemming
+def snowballStemming():
+    corpus=[]
+    ss = SnowballStemmer(language='english')
+
+    for i in range(0,len(tokens)):
+        stemtitle=[]
+        for word in tokens[i][0]:
+            stemtitle.append(ss.stem(word))
+        stemreview=[]
+        for word in tokens[i][1]:
+            stemreview.append(ss.stem(word))
+        corpus.append((' '.join(stemtitle),' '.join(stemreview)))
+
+def porterstemming():
+    corpus=[]
+    ss = PorterStemmer(language='english')
+
+    for i in range(0,len(tokens)):
+        stemtitle=[]
+        for word in tokens[i][0]:
+            stemtitle.append(ss.stem(word))
+        stemreview=[]
+        for word in tokens[i][1]:
+            stemreview.append(ss.stem(word))
+        corpus.append((' '.join(stemtitle),' '.join(stemreview)))
+
+#lemmatizing
+def lemmatizing():
+    corpus=[]
+    wnl = WordNetLemmatizer()
+
+    for i in range(0,len(tokens)):
+        stemtitle=[]
+        for word in tokens[i][0]:
+            stemtitle.append(wnl.lemmatize(word))
+        stemreview=[]
+        for word in tokens[i][1]:
+            stemreview.append(wnl.lemmatize(word))
+        corpus.append((' '.join(stemtitle),' '.join(stemreview)))
+
+#transform words into features / bag of words
+def vectorize():
+    vect = TfidfVectorizer(min_df=2, max_df=0.5, ngram_range=(1,2))#see if it is too much
+    features = vect.fit_transform([tup[0]+tup[1] for tup in corpus]).toarray()
+    #features_review = vect.fit_transform([tup[1] for tup in corpus])
+    #pd.DataFrame(features_review.todense(),columns=vect.get_feature_names())
+
+
+#create model
+
+def naiveBayes(X_train, y_train, X_test):
+    classifier = GaussianNB()
+    model = classifier.fit(X_train, y_train)
+
+    y_pred = model.predict(X_test)
+    return y_pred
+
+def decisionTree(X_train, y_train, X_test):
+    classifier = DecisionTreeClassifier(min_samples_leaf=10)
+    model = classifier.fit(X_train, y_train)
+
+    # tree.plot_tree(classifier) 
+
+    y_pred = model.predict(X_test)
+
+    return y_pred 
+
+def SVM(X_train, y_train, X_test):
+    classifier = SVC()
+    classifier.fit(X_train, y_train)
+    y_pred = classifier.predict(X_test)
+
+    return y_pred
+
+def knn(X_train, y_train, X_test):
+    classifier = NearestNeighbors()
+    classifier.fit(X_train, y_train)
+    y_pred = classifier.predict(X_test)
+
+    return y_pred
+
+#performance results
+def evaluatePerformance(y_test, y_pred):
+    print(confusion_matrix(y_test, y_pred))
+    print('Accuracy: ', accuracy_score(y_test, y_pred))
+    print('Precision: ', precision_score(y_test, y_pred))
+    print('Recall: ', recall_score(y_test, y_pred))
+    print('F1: ', f1_score(y_test, y_pred))
+
